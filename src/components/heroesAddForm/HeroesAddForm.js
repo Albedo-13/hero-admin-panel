6a819ecaa3de573dc76a31a8d6d56f6bd1f65303
PavelProkopenya <pavel12minsk@mail.ru>
2023-07-18 15:56:57 +0300

    feat: create selectlist options dynamically

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,11 +1,9 @@
 // Дополнительно:
-// TODO: Элементы <option></option> желательно сформировать на базе
-// данных из фильтров
 // TODO: Валидация полей формы
 // TODO: Анимации
 
 import { useDispatch, useSelector } from "react-redux";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 import { heroCreated } from "../../actions";
 import { useHttp } from "../../hooks/http.hook";
@@ -13,7 +11,7 @@ import { useHttp } from "../../hooks/http.hook";
 const HeroesAddForm = () => {
   const dispatch = useDispatch();
   const { request } = useHttp();
-  const { filters } = useSelector(state => state);
+  const { filters } = useSelector((state) => state);
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -28,43 +26,41 @@ const HeroesAddForm = () => {
     request("http://localhost:3001/heroes", "POST", JSON.stringify(newHero))
       .then((data) => dispatch(heroCreated(data)))
       .then(() => clearFormFields(e))
-      .then(err => console.log(err));
-  }
+      .then((err) => console.log(err));
+  };
 
   const clearFormFields = (e) => {
     e.target.name.value = "";
     e.target.text.value = "";
     e.target.element.value = "DEFAULT";
-  }
+  };
 
   const renderSelectList = (filters) => {
+    console.log(filters);
+
+    const optionsList = filters.map(({ name, label }) => {
+      // eslint-disable-next-line array-callback-return
+      if (name === "DEFAULT") return;
+      return (
+        <option key={name} value={name}>
+          {label}
+        </option>
+      );
+    });
+
     return (
-      <select
-        required
-        className="form-select"
-        id="element"
-        name="element"
-        defaultValue={"DEFAULT"}>
+      <select required className="form-select" id="element" name="element" defaultValue={"DEFAULT"}>
         <option value="DEFAULT" disabled>Я владею элементом...</option>
-        <option value="fire">Огонь</option>
-        <option value="water">Вода</option>
-        <option value="wind">Ветер</option>
-        <option value="earth">Земля</option>
+        {optionsList}
       </select>
-    )
-  }
+    );
+  };
 
   return (
     <form onSubmit={(e) => onSubmit(e)} className="border p-4 shadow-lg rounded">
       <div className="mb-3">
         <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
-        <input
-          required
-          type="text"
-          name="name"
-          className="form-control"
-          id="name"
-          placeholder="Как меня зовут?" />
+        <input required type="text" name="name" className="form-control" id="name" placeholder="Как меня зовут?" />
       </div>
 
       <div className="mb-3">
@@ -75,7 +71,8 @@ const HeroesAddForm = () => {
           className="form-control"
           id="text"
           placeholder="Что я умею?"
-          style={{ "height": '130px' }} />
+          style={{ height: "130px" }}
+        />
       </div>
 
       <div className="mb-3">
@@ -85,7 +82,7 @@ const HeroesAddForm = () => {
 
       <button type="submit" className="btn btn-primary">Создать</button>
     </form>
-  )
-}
+  );
+};
 
 export default HeroesAddForm;
